a40c4905606e6651498e86250df522425af0df0b
Shank <shashanksharma21@gmail.com>
2023-10-26 16:03:43 -0400

    wip to continue on apple (because the wifi here sucks)

--- pkg_manager/package_manager.py
+++ pkg_manager/package_manager.py
@@ -5,11 +5,11 @@ import jsonpickle
 import json
 from .source_file_parser import parse_all_entries
 
-logging.basicConfig(format='%(asctime)s-%(levelname)s-%(message)s', level=logging.DEBUG)
+logging.basicConfig(format="%(asctime)s-%(levelname)s-%(message)s", level=logging.DEBUG)
 
 
 class PackageManager:
-    '''
+    """
     The main class that manages all the packages.
     This class has following members of interest.
 
@@ -23,13 +23,14 @@ class PackageManager:
 
         reverse_dependency_map (map: str -> list): Reverse dependency map i.e., map of a dependency and all the package
                                 that depend on the dependency.
-    '''
+    """
+
     def __init__(self, source_file, base_url):
-        '''
+        """
         Create package manager object.
         :param source_file: Path to the source file.
         :param base_url: Base mirror url.
-        '''
+        """
         self.source_file_path = source_file
         self.base_url = base_url
         self.all_pkg_entries = {}
@@ -38,14 +39,16 @@ class PackageManager:
 
     @staticmethod
     def from_picked_json(pickled_json):
-        '''
+        """
         Create PackageManager object from the saved json file.
         Note that this should be saved before using dump_to_pickled_json method.
         :param pickled_json: Local path to the picked json file.
         :return: None
-        '''
+        """
         if not os.path.exists(pickled_json):
-            logging.error("Provided pickled json file %s does not exist. Exiting!", pickled_json)
+            logging.error(
+                "Provided pickled json file %s does not exist. Exiting!", pickled_json
+            )
             sys.exit(-1)
         fp = open(pickled_json, "r")
         all_conts = fp.read()
@@ -66,16 +69,18 @@ class PackageManager:
         logging.info("Finished building dependency map.")
 
     def build_pkg_entries(self) -> None:
-        '''
+        """
         Build an index of all the package entries from the provided source file.
         :return: None
-        '''
+        """
         logging.info("Trying to build package entries from %s", self.source_file_path)
         if os.path.exists(self.source_file_path):
             fp = open(self.source_file_path, "r")
             all_lines = fp.readlines()
             fp.close()
-            logging.debug("Parsing pkg entries from source file:" + self.source_file_path)
+            logging.debug(
+                "Parsing pkg entries from source file:" + self.source_file_path
+            )
             all_entries = parse_all_entries(all_lines, self.base_url)
             for ce in all_entries:
                 self.all_pkg_entries[ce.pkg_name] = ce
@@ -85,40 +90,44 @@ class PackageManager:
             logging.error("Unable to open the provided file %s", self.source_file_path)
 
     def get_pkgs_with_dependency(self, dep_name):
-        '''
+        """
         Get all packages that contain the provided dependency.
         :param dep_name: Dependency name
         :return: list of package names that contain the provided dependency.
-        '''
+        """
         dep_name = dep_name.lower()
         all_rev_deps = set(self.reverse_dependency_map.keys())
         all_rel_deps = list(filter(lambda x: dep_name in x.lower(), all_rev_deps))
         all_interesting_pkgs = set()
         for curr_d in all_rel_deps:
             all_interesting_pkgs.update(set(self.reverse_dependency_map[curr_d]))
-        all_interesting_pkgs = list(filter(lambda x: dep_name in x.lower(), all_interesting_pkgs))
+        all_interesting_pkgs = list(
+            filter(lambda x: dep_name in x.lower(), all_interesting_pkgs)
+        )
         return list(all_interesting_pkgs)
 
     def get_pkgs_without_dependency(self, dep_name):
-        '''
+        """
         Get all packages that DO NOT contain the provided dependency.
         :param dep_name: Dependency name
         :return: list of package names that DO NOT contain the provided dependency.
-        '''
+        """
         dep_name = dep_name.lower()
         all_rev_deps = set(self.reverse_dependency_map.keys())
         all_rel_deps = list(filter(lambda x: dep_name not in x.lower(), all_rev_deps))
         all_interesting_pkgs = set()
         for curr_d in all_rel_deps:
             all_interesting_pkgs.update(set(self.reverse_dependency_map[curr_d]))
-        all_interesting_pkgs = list(filter(lambda x: dep_name not in x.lower(), all_interesting_pkgs))
+        all_interesting_pkgs = list(
+            filter(lambda x: dep_name not in x.lower(), all_interesting_pkgs)
+        )
         return list(all_interesting_pkgs)
 
     def rebuild_pkg_entries(self) -> None:
-        '''
+        """
         Rebuild package entries.
         :return: None
-        '''
+        """
         # clear out everything
         self.all_pkg_entries.clear()
         self.dependency_map.clear()
@@ -128,11 +137,11 @@ class PackageManager:
         self.build_pkg_entries()
 
     def dump_to_pickled_json(self, path_to_json):
-        '''
+        """
         Dump this package manager object to a json file that can be loaded later.
         :param path_to_json: Local file path where the json should be stored.
         :return: None
-        '''
+        """
         json_str = jsonpickle.encode(self)
         json_obj = json.loads(json_str)
         fp = open(path_to_json, "w")
@@ -140,13 +149,13 @@ class PackageManager:
         fp.close()
 
     def download_package_source(self, pkg_name, output_folder) -> None:
-        '''
+        """
         Download all the files that are part of the sources of the provided package
          into a local folder.
         :param pkg_name: Package name whose contents needs to be downloaded.
         :param output_folder: Local file path where the files should be stored.
         :return: None
-        '''
+        """
         if pkg_name not in self.all_pkg_entries:
             logging.error("Unrecognized package %s", pkg_name)
         else:
@@ -155,21 +164,25 @@ class PackageManager:
             curr_dir = os.getcwd()
             if not os.path.exists(output_folder):
                 os.makedirs(output_folder)
-            logging.info("Downloading %d files to %s using wget", len(pkg_en.source_urls), output_folder)
+            logging.info(
+                "Downloading %d files to %s using wget",
+                len(pkg_en.source_urls),
+                output_folder,
+            )
             os.chdir(output_folder)
             for curr_f in pkg_en.source_urls:
-                os.system("wget " + curr_f)
+                os.system("wget -c " + curr_f)
             os.chdir(curr_dir)
             logging.info("Finished downloading files for package %s", pkg_name)
 
     def download_packages_source(self, pkgs, output_folder) -> None:
-        '''
+        """
         Download all the files of all the packages in the given list of packages
         into a local folder.
         :param pkgs: List of packages that need to be downloaded.
         :param output_folder: Path to the local folder where the files of the provided package need to be stored.
         :return: None
-        '''
+        """
         logging.info("Downloading %d packages to %s", len(pkgs), output_folder)
         for curr_pkg in pkgs:
             pkg_folder = os.path.join(output_folder, curr_pkg)
@@ -177,9 +190,9 @@ class PackageManager:
         logging.info("Finished downloading %d packages to %s", len(pkgs), output_folder)
 
     def download_all_packages_source(self, output_folder) -> None:
-        '''
+        """
         Download sources of all the packages in to the provided folder.
         :param output_folder: Path to the local folder where the files should be downloaded.
         :return: None
-        '''
+        """
         self.download_packages_source(self.all_pkg_entries.keys(), output_folder)
---
+++ the100_download_sources.py
@@ -0,0 +1,264 @@
+##########################################################################
+#
+# This script will download all the C/C++ package tar sources to a local
+# folder -> extract, build and install the tar sources -> extract the .bc
+# files from the installed archives using wllvm
+#
+# invoke this script with sudo, so that packages can be installed
+#
+# Make sure that clang and llvm-link are set to the same llvm version before
+# invoking this script.
+#
+##########################################################################
+from pkg_manager import PackageManager
+from ctypes.util import find_library
+import os
+import subprocess
+
+# import sys
+# import re
+
+# make sure paths are set correctly
+# path the the mirror file
+source_file_to_read_packages_from = (
+    "/home/shank/code/research/DetectERR/the100/apt-scraper-utils/Sources-JammyMain"
+)
+
+# mirror url
+mirror_url = "http://mirror.math.ucdavis.edu/ubuntu/"
+
+# sources will be downloaded here
+local_download_folder_for_sources = (
+    "/home/shank/code/research/DetectERR/the100/apt-scraper-utils/apt_scraper_sources"
+)
+
+# script will extract and build it here
+extracted_tar_sources = (
+    "/home/shank/code/research/DetectERR/the100/apt-scraper-utils/extracted_tar_sources"
+)
+
+# bitcodes will here extracted here
+afl_fuzzing_sources = (
+    "/home/shank/code/research/DetectERR/the100/apt-scraper-utils/afl_sources"
+)
+
+if not os.path.isdir(local_download_folder_for_sources):
+    cmd = "(" + "mkdir " + local_download_folder_for_sources + ")"
+    print(f"executing>> {cmd}")
+    subprocess.call(cmd, shell=True)
+
+if not os.path.isdir(afl_fuzzing_sources):
+    cmd = "(" + "mkdir " + afl_fuzzing_sources + ")"
+    print(f"executing>> {cmd}")
+    subprocess.call(cmd, shell=True)
+
+if not os.path.isdir(extracted_tar_sources):
+    cmd = "(" + "mkdir " + extracted_tar_sources + ")"
+    print(f"executing>> {cmd}")
+    subprocess.call(cmd, shell=True)
+
+
+# p = PackageManager(source_file_to_read_packages_from, mirror_url)
+# p.build_pkg_entries()
+#
+# p.dump_to_pickled_json("dump_jammy_main.picked.json")
+p = PackageManager.from_picked_json("dump_jammy_main.picked.json")
+
+
+packages_available = p.all_pkg_entries
+
+# for making package installation noninterative
+cmd = "(" + "export DEBIAN_FRONTEND=noninteractive" + ")"
+print(f"executing>> {cmd}")
+subprocess.call(cmd, shell=True)
+
+
+# total download count
+dl_cnt = 0
+
+# checked deps
+checked_available = set()
+
+for pkgs in packages_available:
+    reverse_dependencies = []
+    dependency_list = p.dependency_map[pkgs]
+    for dependencies in dependency_list:
+        if dependencies not in checked_available:
+            print(f">> dep:{dependencies} already checked")
+            subprocess.call(["sudo apt -yq install", str(dependencies)], shell=True)
+            checked_available.add(dependencies)
+        # install all reverse dependencies might be too slow (not necessary), uncomment if needed
+        # for reverse_deps in p.reverse_dependency_map[dependencies]:
+        #    subprocess.call(['sudo apt -yq install', str(reverse_deps)], shell=True)
+        reverse_dependencies.extend(p.reverse_dependency_map[dependencies])
+
+    print()
+    print("...")
+    print("DOWNLOADING " + str(pkgs) + "from the mirror...")
+    print("...")
+    p.download_package_source(pkgs, local_download_folder_for_sources)
+    dl_cnt += 1
+    print(f">> dl_cnt={dl_cnt}")
+
+    # for lib in reverse_dependencies:
+    #     if find_library(lib) != None:
+    #         print()
+    #         print("...")
+    #         print("DOWNLOADING " + str(pkgs) + "from the mirror...")
+    #         print("...")
+    #         p.download_package_source(pkgs, local_download_folder_for_sources)
+    #         dl_cnt += 1
+    #         print(f">> dl_cnt={dl_cnt}")
+    #         break
+
+    # all_dep_libs_available = True
+    # print(f">> len(reverse_dependencies)={len(reverse_dependencies)}")
+    # for lib in reverse_dependencies:
+    #     print(f">> checking for lib:{lib}")
+    #     if find_library(lib) is None:
+    #         all_dep_libs_available = False
+    #         break
+    #
+    # if all_dep_libs_available:
+    #     print()
+    #     print("...")
+    #     print("DOWNLOADING " + str(pkgs) + "from the mirror...")
+    #     print("...")
+    #     p.download_package_source(pkgs, local_download_folder_for_sources)
+    #     dl_cnt += 1
+    #     print(f">> dl_cnt={dl_cnt}")
+
+    if dl_cnt == 300:
+        print(f">> downloaded {dl_cnt} packages")
+        break
+
+
+success_cnt = 0
+# Extract the tar sources, build them and install and put them in another folder
+for subdir, dirs, files in os.walk(local_download_folder_for_sources):
+
+    for File in files:
+        if ".orig." in str(File):
+
+            # extract the archive
+            cmd = (
+                "("
+                + "cd "
+                + local_download_folder_for_sources
+                + " && "
+                + "tar -xf "
+                + str(File)
+                + ")"
+            )
+            print(f"executing>> {cmd}")
+            subprocess.call(cmd, shell=True)
+
+            archive_parts = File.split(".orig")
+            underscore_split = archive_parts[0].split("_")
+
+            directory_name = ""
+            try:
+                directory_name = (
+                    str(underscore_split[0]) + "-" + str(underscore_split[1])
+                )
+            except:
+                continue
+
+            configure_path = (
+                local_download_folder_for_sources
+                + "/"
+                + directory_name
+                + "/"
+                + "configure"
+            )
+
+            # check if a configure script exists in the directory
+            if os.path.exists(configure_path) == True:
+                print(File)
+                # now cd into the archive
+                # cmd = (
+                #     "("
+                #     + f"cd {local_download_folder_for_sources}/{directory_name}"
+                #     + " && "
+                #     + "export LLVM_COMPILER=clang"
+                #     + " && "
+                #     + "export WLLVM_OUTPUT_LEVEL=Debug"
+                #     + " && "
+                #     + f"export WLLVM_OUTPUT_FILE=/tmp/{str(File)}_wllvm.log"
+                #     + " && "
+                #     + "CC=wllvm CXX=wllvm++ yes '' | ./configure"
+                #     + " && "
+                #     + "make CC=wllvm CXX=wllvm++ -j12"
+                #     + " && "
+                #     + f"make CC=wllvm CXX=wllvm++ install DESTDIR={extracted_tar_sources}/{directory_name}"
+                #     + ")"
+                # )
+                cmd = (
+                    "("
+                    + f"cd {local_download_folder_for_sources}/{directory_name}"
+                    + " && "
+                    + "CC=clang CXX=clang++ CFLAGS='-g -O0' CXXFLAGS='-g -O0' yes '' | ./configure"
+                    + " && "
+                    + "make CC=clang CXX=clang++ -j12"
+                    + " && "
+                    + f"make CC=clang CXX=clang++ install DESTDIR={extracted_tar_sources}/{directory_name}"
+                    + ")"
+                )
+                print(f"executing>> {cmd}")
+                res = subprocess.call(cmd, shell=True)
+                if res == 0:
+                    print(f">> success")
+                    success_cnt += 1
+                else:
+                    print(f">> failed")
+
+                if success_cnt == 100:
+                    print(f">> success_cnt={str(success_cnt)}")
+                    break
+
+# # run extract-bc to get the bit-code files form the binaries
+# for subdir, dirs, files in os.walk(extracted_tar_sources):
+#
+#     if subdir.endswith("/bin"):
+#
+#         for File in files:
+#             cmd = (
+#                 "("
+#                 + "cd "
+#                 + subdir
+#                 + " && "
+#                 + "extract-bc "
+#                 + File
+#                 + " && "
+#                 + "mv "
+#                 + File
+#                 + ".bc "
+#                 + afl_fuzzing_sources
+#                 + ")"
+#             )
+#             print(f"executing>> {cmd}")
+#             subprocess.call(cmd, shell=True)
+#
+# # run extract-bc to get bitcodes from library files if any
+# for subdir, dirs, files in os.walk(extracted_tar_sources):
+#
+#     if subdir.endswith("/lib"):
+#
+#         for File in files:
+#             if (".a") in File:
+#                 cmd = (
+#                     "("
+#                     + "cd "
+#                     + subdir
+#                     + " && "
+#                     + "extract-bc -b "
+#                     + File
+#                     + " && "
+#                     + "mv "
+#                     + File
+#                     + ".bc "
+#                     + afl_fuzzing_sources
+#                     + ")"
+#                 )
+#                 print(f"executing>> {cmd}")
+#                 subprocess.call(cmd, shell=True)
